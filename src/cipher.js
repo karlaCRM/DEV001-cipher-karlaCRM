c73010c7b3e034db74385c45ab5d6e422e1dee94
karlaCRM <abraxaskrm@gmail.com>
2022-10-05 17:39:18 -0500

    terminado, falta agregar readme

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,90 @@
 const cipher = {
-  // ...
-};
+    encode (offset, stringO){
+
+        if(offset== null || stringO==[]){
+            throw new TypeError("No has ingresado texto");
+        }
+
+        let letter= stringO;
+        //console.log (letter)
+        let resultadoUno= ""; //resultado letra nueva
+        let formula = "";
+        let formulaDos= "";
+        let toAscii=""
+        let formulaTres=""
+
+        
+        
+
+        for(let i = 0; i<letter.length; i++){
+            toAscii= letter.charCodeAt(i);
+            //console.log(toAscii);
+
+            if (toAscii>=65 && toAscii<=90){ //condicion para realizar el cambio
+                formula = String.fromCharCode((toAscii- 65 + offset)%26 + 65);//MAYUSCULAS
+                resultadoUno += formula;
+                //console.log (resultadoUno)
+            } else if (toAscii>=97 && toAscii<=122){ 
+                formulaDos = String.fromCharCode((toAscii- 97 + offset)%26 + 97);//minusculas
+                resultadoUno += formulaDos;
+                //console.log (resultadoUno)
+            } else if (toAscii===241){ //ñ
+                formulaTres= String.fromCharCode((toAscii-0 + offset)-offset).replace("ñ", "%")
+                resultadoUno = resultadoUno += formulaTres
+                //console.log(resultadoUno)
+
+            } else if (toAscii===209){ //Ñ
+                formulaTres= String.fromCharCode((toAscii-0 + offset)-offset).replace("Ñ", "#")
+                resultadoUno = resultadoUno += formulaTres
+                //console.log(resultadoUno)
+            } else {
+                resultadoUno = resultadoUno +  letter[i]
+            }
+            
+            }
+         return resultadoUno;
+}   
+    ,decode (offsetTwo,codToUncode){
+       
+        if(offsetTwo== null || codToUncode==[]){
+            throw new TypeError("No has pegado texto");
+        }
+
+        let textCode = codToUncode; //texto recibido
+        //console.log(codToUncode)
+        let resultadoDos=""; 
+        let formulaRegreso= "";
+        let toAsciiDos= "";
+        let formulaSymbols="";
+
+        for(let a=0; a<textCode.length; a++){
+            toAsciiDos= parseInt(textCode.charCodeAt(a));
+            //console.log(toAsciiDos); //convertido a ascii
+
+            if (toAsciiDos>=65 && toAsciiDos<=90){ //condiciones para realizar la formula MAYUSCULAS
+                formulaRegreso= String.fromCharCode((toAsciiDos -65 - offsetTwo% 26 + 26) % 26 + 65);
+                resultadoDos += formulaRegreso
+                //console.log (resultadoDos)
+            }else if (toAsciiDos>=97 && toAsciiDos<=122){ //MINUSCULAS
+                formulaRegreso = String.fromCharCode((toAsciiDos - 97 - offsetTwo % 26 + 26)%26 + 97);
+                resultadoDos += formulaRegreso;
+                //console.log (resultadoDos)
+            } else if (toAsciiDos===37){ //ñ
+                formulaSymbols= String.fromCharCode((toAsciiDos-0 + offsetTwo)-offsetTwo).replace("%", "ñ")
+                resultadoDos = resultadoDos += formulaSymbols
+                //console.log(resultadoDos)
+
+            } else if (toAsciiDos===35){ //Ñ
+                formulaSymbols= String.fromCharCode((toAsciiDos-0 + offsetTwo)-offsetTwo).replace("#", "Ñ")
+                resultadoDos = resultadoDos += formulaSymbols
+                //console.log(resultadoDos)
+            } else {
+                resultadoDos = resultadoDos +  textCode[a]}
+            }
+            return resultadoDos
+        }
+    }
+
+
 
 export default cipher;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,36 @@
 import cipher from './cipher.js';
+let containerResult = document.getElementById('iResult');
+document.getElementById("encrypt-btn").addEventListener("click", cifrar)
 
-console.log(cipher);
+
+function cifrar(){
+   const stringO = document.getElementById("iOriginal").value;
+   //console.log (stringO)
+   const offset = parseInt(document.getElementById("shift").value);
+   //console.log (offset)
+   containerResult.innerHTML= cipher.encode(offset,stringO);
+
+   }
+    
+let containerDecode = document.getElementById("resultDecode")
+document.getElementById("decode-btn").addEventListener("click", descifrar)
+
+function descifrar(){
+    const codToUncode = document.getElementById("codificado").value;
+    //console.log (codToUncode)
+    const offsetTwo = parseInt(document.getElementById("shiftBack").value);
+    //console.log (offsetTwo)
+    containerDecode.innerHTML= cipher.decode(offsetTwo, codToUncode);
+}
+
+document.getElementById("copyToClipBoard").addEventListener("click", copyToClipBoard)
+function copyToClipBoard() {
+    
+    containerResult.select();
+    document.execCommand("copy");
+
+    alert("Tu texto codificado se encuentra en el portapapeles, ya solo debes pegarlo en el whats <3");
+
+}
+
+//console.log(cipher);
